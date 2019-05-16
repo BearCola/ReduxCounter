b854a3e10ff70e057d675df47f853b4784bc824d
BearCola <sangin84@gmail.com>
2019-05-16 18:35:03 +0900

    redux complete

--- containers/App.js
+++ containers/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-import CounterContainer from "./CounterContainer"
+import React, { Component } from "react";
+import CounterListContainer from "./CounterListContainer";
+import getRandomColor from "../lib/getRandomColor";
+
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Buttons from "../components/Buttons";
 
 class App extends Component {
-  render() {
-    return (
-        <CounterContainer />
-    )
-  }
+    render() {
+        const { onCreate, onRemove } = this.props;
+        return (
+            <div className="App">
+              <Buttons
+                onCreate={onCreate}
+                onRemove={onRemove}
+              />
+                <CounterListContainer />
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapToDispatch = (dispatch) => ({
+  onCreate: () => dispatch(actions.create(getRandomColor())),
+  onRemove: () => dispatch(actions.remove())
+});
+
+export default connect(null, mapToDispatch)(App);
